7c9e98399743dc54741bdfaf7fa96b112442be65
Luis Carrazco <luiscarrazco@gmail.com>
2019-02-17 20:32:55 -0700

    vanishing dice for now

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
@@ -19,3 +19,26 @@ dice = Math.floor(Math.random() * 6) + 1
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

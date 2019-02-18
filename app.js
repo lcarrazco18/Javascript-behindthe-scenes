700c5dce20c2d345fe84cba32b1cde01ce369488
Luis Carrazco <luiscarrazco@gmail.com>
2019-02-17 20:15:13 -0700

    first DOM manipulation and adding eslint to project

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer, dice;
 
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
 
+dice = Math.floor(Math.random() * 6) + 1
 
-
-
-
-
-
-
-
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';

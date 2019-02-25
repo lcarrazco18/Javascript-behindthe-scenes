47a4da511679b3a27f7e86b40ec0e039763769e6
Luis Carrazco <luiscarrazco@gmail.com>
2019-02-25 09:26:24 -0700

    double 6s reset score

--- app2.js
+++ app2.js
@@ -1,9 +1,19 @@
 /* eslint-disable indent */
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-var lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
@@ -16,12 +26,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         diceDOM.src = 'dice-' + dice + '.png';
 
         // 3. Update the round score IF the rolled number was NOT a 1
-        if (dice === 6 && lastDice === 6) {
-            // Player looses score
-            scores[activePlayer] = 0;
-            document.querySelector('#score-' + activePlayer).textContent = '0';
-            nextPlayer();
-        } else if (dice !== 1) {
+        if (dice !== 1) {
             // add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -29,8 +34,6 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
             // next player
             nextPlayer();
         }
-
-        lastDice = dice;
     }
 });
 
